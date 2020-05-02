1d8d39e97ce3d1ed6c22c2b90ee5d926cc73998b
samirghouri <samira.ghouri@st.niituniversity.in>
2020-05-02 13:40:54 +0530

    changed min_heap file

--- Priority_Queues/min_binary_heap.py
+++ Priority_Queues/min_binary_heap.py
@@ -1,71 +1,69 @@
-# Implementing a Min Binary Heap
-
-
-class BinHeap():
-
-    # since the entire binary heap can be represented by a single list
-    # we use all the constuctor to initialise the list.
+class BinaryHeap:
     def __init__(self):
-        self.list = None  # assigning zero just to make the start index 1
-        self.current_size = None
+        # added 0 as we want the heap list to have start index 1
+        self.heapList = [0]
 
-    # inserting in the heap but first
-    # we need to nulllify the violation of the invariance
+    def insert(self, element):
+        """
+        --Most efficient way to insert in a heap is to append at the end of the heap.
+        --This will violate the heap structure property but it can be taken care of. 
+        """
+        self.heapList.append(element)
+        index_of_element = self.heapList.index(element)
+        self.swim(index_of_element)
 
     def swim(self, i):
-        while i//2 > 0:
-            # i//2 represents the parent node
-            # if the child node is smaller than the parent node
-            if self.list[i] < self.list[i//2]:
-                self.list[i], self.list[i//2] = self.list[i//2], self.list[i]
-                i = i//2
+        while i >= 2:
+            if self.heapList[i] < self.heapList[i//2]:
+                self.heapList[i], self.heapList[i //
+                                                2] = self.heapList[i//2], self.heapList[i]
+            i = i//2
 
-    def insert(self, k):
-        self.list.append(k)
-        self.current_size = self.current_size+1
-        self.swim(self.current_size)
-
-    # Now we will look at when we what to delete the root node
-    # we put the last node in place of the root node and sink it down
-    # Before that we also need to maintain the invariance
+    def delete_min(self):
+        """
+        --Most efficient way to delete min is to remove the root elelment and replace it with an element at the end
+        --This will again distort the heap structure so we will sink it down
+        """
+        min_element = self.heapList[1]
+        last_element_index = len(self.heapList)-1
+        self.heapList[1] = self.heapList[last_element_index]
+        self.heapList.pop()
+        self.sink(1)
+        return min_element
 
     def sink(self, i):
-        while i*2 < self.current_size:
-            min_chd = self.minChild(i)
-            if self.list[i] > self.list[min_chd]:
-                self.list[i], self.list[min_chd] = self.list[min_chd], self.list[i]
-            i = min_chd
+        list_length = len(self.heapList)-1
+        while 2*i <= list_length:
+            min_child = self.min_child(i)
+            if self.heapList[i] > self.heapList[min_child]:
+                self.heapList[i], self.heapList[min_child] = self.heapList[min_child], self.heapList[i]
+            i = min_child
 
-    def minChild(self, i):
-        if i*2+1 > self.current_size:
-            return i*2
+    def min_child(self, i):
+        list_length = len(self.heapList)-1
+        if 2*i+1 > list_length:
+            return 2*i
         else:
-            if self.list[2*i] < self.list[i*2+1]:
-                return i*2
+            if self.heapList[2*i] < self.heapList[2*i+1]:
+                return 2*i
             else:
-                return i*2+1
+                return 2*i+1
 
-    def delMin(self):
-        minimum = self.list[1]
-        self.list[1] = self.list[self.current_size]
-        self.current_size = self.current_size-1
-        self.list.pop()
-        self.sink(1)
-        return minimum
-
-    def display(self):
-        return self.list[1:]
-
-    def build_heap(self, array):
-        self.list = [0]+array
-        self.current_size = len(array)
-        i = self.current_size//2
+    def build_heap(self, given_list):
+        i = len(given_list)//2
+        self.heapList += given_list
         while i > 0:
             self.sink(i)
             i = i-1
 
+    def display(self):
+        print(self.heapList[1:])
 
-a = [5, 2, 6, 3, 1, 97, 4]
-obj = BinHeap()
+
+obj = BinaryHeap()
+a = [1, 4, 3, 6, 8, 2]
 obj.build_heap(a)
-print(obj.display())
+obj.display()
+obj.insert(2)
+obj.insert(7)
+obj.display()
